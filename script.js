565cb9ac0261b28e21e2328742266ddd81631614
Susan Choi <2539lee@gmail.com>
2022-03-25 21:56:55 -0700

    Filter fetched posts

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const postsContainer = document.getElementById('posts-container')
 const filter = document.getElementById('filter')
 const loading = document.querySelector('.loader')
 
-let limit = 3
+let limit = 5
 let page = 1
 
 // Fetch posts from API
@@ -34,5 +34,46 @@ async function showPosts() {
   })
 }
 
+// Show loader & fetch more posts
+function showLoading() {
+  loading.classList.add('show')
+
+  setTimeout(() => {
+    loading.classList.remove('show')
+
+    setTimeout(() => {
+      page++
+      showPosts()
+    }, 300)
+  }, 1000)
+}
+
+// Filter posts by input
+function filterPosts(e) {
+  const term = e.target.value.toUpperCase()
+  const posts = document.querySelectorAll('.post')
+
+  posts.forEach((post) => {
+    const title = post.querySelector('.post-title').innerText.toUpperCase()
+    const body = post.querySelector('.post-body').innerText.toUpperCase()
+
+    if (title.indexOf(term) > -1 || body.indexOf(term) > -1) {
+      post.style.display = 'flex'
+    } else {
+      post.style.display = 'none'
+    }
+  })
+}
+
 // Show initial posts
 showPosts()
+
+window.addEventListener('scroll', () => {
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement
+
+  if (scrollTop + clientHeight >= scrollHeight - 5) {
+    showLoading()
+  }
+})
+
+filter.addEventListener('input', filterPosts)
